2c1ea6e940a41ed2cb343dcf65f01b1ce976ca65
gonghuan <gonghuan@fancydigital.com.cn>
2019-08-06 13:23:48 +0800

    ci

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cp -rf ./vim_config/vim/* ./vim_config/vim/.* ~/.vim/
 cp -rf ./vim_config/vimrc ~/.vimrc
 
 # fix error cuased by lc
-echo "export LC_CTYPE=en_US.UTF-8" >> ~/.bashrc
-echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
+#echo "export LC_CTYPE=en_US.UTF-8" >> ~/.bashrc
+#echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
