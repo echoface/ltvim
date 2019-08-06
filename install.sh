4a8ab7b75ef4f2c798a57461cb83a83a1fb79f0b
gonghuan <gonghuan@fancydigital.com.cn>
2019-08-06 13:26:00 +0800

    rename folder

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-mkdir ~/.vim
-cp -rf ./vim_config/vim/* ./vim_config/vim/.* ~/.vim/
-cp -rf ./vim_config/vimrc ~/.vimrc
+CurDir=$(cd `dirname $0`; pwd)
+cd ${CurDir}
+
+echo `pwd`
+
+if [ -d "~/.vim" ]; then
+  echo "backup ~/.vim folder..."
+  mv ~/.vim ~/.vim.bak
+fi
+
+if [ -f "~/.vimrc" ]; then
+  echo "backup ~/.vimrc file..."
+  mv ~/.vimrc ~/.vimrc.bak
+fi
+
+echo "install .... ...."
+cp -rf ./config/vim  ~/.vim
+cp -rf ./config/vimrc ~/.vimrc
 
 # fix error cuased by lc
 #echo "export LC_CTYPE=en_US.UTF-8" >> ~/.bashrc
