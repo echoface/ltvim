b6f33b4b65cc1cb331ce15f9bfe79bc62fa4a61f
gonghuan <gonghuan@fancydigital.com.cn>
2019-12-03 10:35:20 +0800

    ciu

--- script/compile_without_gui.sh
+++ script/compile_without_gui.sh
@@ -1,28 +1,17 @@
-./configure  --prefix=$HOME \
-  --with-features=huge \
-  --enable-pythoninterp=yes \
-  --enable-cscope\
-  --enable-fail-if-missing \
-  --enable-multibyte \
-  --enable-fontset \
-  --with-compiledby="HuanGong" \
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/\
-  --disable-gui \
-  --without-x
-
-./configure --prefix=$HOME \
-  --with-features=huge \
-  --enable-multibyte \
-  --enable-rubyinterp=yes \
-  --enable-perlinterp=yes \
-  --enable-luainterp=yes \
-  --enable-pythoninterp=yes \
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-  --enable-python3interp=yes \
-  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-  --enable-cscope \
-  --disable-gui \
-  --without-x
+./configure --prefix=$HOME --with-features=huge \
+            --without-x \
+            --disable-gui \
+            --enable-cscope \
+            --enable-multibyte \
+            --enable-luainterp=yes \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --enable-python3interp=yes \
+            --with-compiledby="HuanGong" \
+            --with-python-command=/usr/bin/python \
+            --with-python3-command=/usr/bin/python3 \
+            --with-python-config-dir=/usr/lib64/python2.7/config \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
 
 make
 make install
