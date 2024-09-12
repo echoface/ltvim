e13293f6c09b49f4501633f4596867af1de39f75
HuanGong <gonghuan.dev@gmail.com>
2024-09-12 11:59:43 +0800

    refactor: goimports and format when save

--- nvim/lua/config/lsp/settings/gopls.lua
+++ nvim/lua/config/lsp/settings/gopls.lua
@@ -1,100 +1,3 @@
-local function apply_action(client, bufnr, action)
-    if action.edit then
-        vim.lsp.util.apply_workspace_edit(action.edit, client.offset_encoding)
-    end
-
-    if not action.command then
-        return
-    end
-
-    local command = type(action.command) == 'table' and action.command or action
-    local fn = client.commands[command.command] or vim.lsp.commands[command.command]
-    if fn then
-        return fn(command, vim.deepcopy({ bufnr = bufnr, client_id = client.id }))
-    end
-
-    local params = {
-        command = command.command,
-        arguments = command.arguments,
-        workDoneToken = command.workDoneToken,
-    }
-    client.request('workspace/executeCommand', params, function(err, r)
-    end, bufnr)
-end
-
-
--- CodeActionCommand
--- FillStruct      = "fill_struct"
--- UndeclaredName  = "undeclared_name"
--- ExtractVariable = "extract_variable"
--- ExtractFunction = "extract_function"
--- ExtractMethod   = "extract_method"
-
--- CodeActionKind
---- ""                          # Empty
---- "quickfix"                  # QuickFix
---- "refactor"                  # Refactor
---- "refactor.extract"          # RefactorExtract
---- "refactor.inline"           # RefactorInline
---- "refactor.rewrite"          # RefactorRewrite
---- "source"                    # Source
---- "source.organizeImports"    # SourceOrganizeImports
---- "source.fixAll"             # SourceFixAll
-
--- codeaction('', 'source.organizeImports')
--- codeaction('apply_fix', 'refactor.rewrite')
-local function code_action(command, only)
-    local clients = vim.lsp.get_clients({ name = 'gopls', bufnr = vim.api.nvim_get_current_buf(), })
-    if #clients == 0 then
-        return
-    end
-    local client = clients[1]
-
-    local params = vim.lsp.util.make_range_params()
-    if command ~= '' and not command:find('gopls') then
-        command = 'gopls.' .. command
-    end
-    if only then
-        params.context = { only = { only } }
-    end
-    local bufnr = vim.api.nvim_get_current_buf()
-
-    local function ca_hdlr(err, result, hdl_ctx, config)
-        if err or not result or next(result) == nil then
-            return
-        end
-
-        local cmd_actions = {}
-        for _, res in pairs(result) do
-            if res.edit or res.command then
-                return apply_action(client, bufnr, res)
-            end
-
-            local act_cmd = res.data and res.data.command or ''
-            if act_cmd == command then
-                table.insert(cmd_actions, res)
-            end
-        end
-
-        if #cmd_actions == 0 then
-            vim.notify('no cmd_actions available', vim.log.levels.INFO)
-            return
-        end
-
-        local action = cmd_actions[1]
-        -- resolve cmd edit action
-        client.request('codeAction/resolve', action, function(_err, resolved_acrtion, ctx, config)
-            if _err then
-                vim.notify("resove fail" .. vim.inspect(err))
-                return
-            end
-            return apply_action(client, bufnr, resolved_acrtion)
-        end, bufnr)
-    end
-
-    client.request('textDocument/codeAction', params, ca_hdlr, bufnr)
-end
-
 -- CodeActionKind
 --- ""                          # Empty
 --- "quickfix"                  # QuickFix
@@ -118,13 +21,28 @@ local function run_specific_code_action(action_kind, filter_func)
     vim.lsp.buf.code_action(opts)
 end
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = "*.go",
-    callback = function()
-        run_specific_code_action("source.organizeImports", nil)
-        vim.lsp.buf.format({ async = false })
+local organize_imports_sync = function(bufnr, isPreflight)
+    local encoding = vim.lsp.util._get_offset_encoding()
+    local params = vim.lsp.util.make_range_params(nil, encoding)
+    params.context = { only = { "source.organizeImports" } }
+
+    if isPreflight then
+        vim.lsp.buf_request(bufnr, "textDocument/codeAction", params, function() end)
+        return
     end
-})
+
+    local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 3000)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, encoding)
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+end
+
 
 M = {}
 M.setup_opts = {
@@ -139,27 +57,31 @@ M.setup_opts = {
 }
 
 -- /*first arg client is ignored */
-M.on_attach = function(_, bufnr)
+M.on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
     local keymap = vim.api.nvim_buf_set_keymap
 
+    organize_imports_sync(bufnr, true)
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+         organize_imports_sync(bufnr, false)
+         vim.lsp.buf.format({async = false})
+      end,
+    })
     vim.api.nvim_buf_create_user_command(bufnr, "GoImports", function()
-        -- code_action("", "source.organizeImports")
         run_specific_code_action("source.organizeImports", nil)
     end, {})
 
     vim.api.nvim_buf_create_user_command(bufnr, "GoFillStruct", function()
-        -- code_action("apply_fix", "refactor.rewrite")
         run_specific_code_action("refactor.rewrite", nil)
     end, {})
+    keymap(bufnr, "i", ",f", "<cmd>:GoFillStruct<cr>", opts)
 
     vim.api.nvim_buf_create_user_command(bufnr, "GoExtractMethod", function()
-        -- code_action("", "refactor.extract")
         run_specific_code_action("refactor.extract", nil)
     end, {})
 
-    keymap(bufnr, "i", ",f", "<cmd>:GoFillStruct<cr>", opts)
 end
-
 
 return M
