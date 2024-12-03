94ca05864e6b3c96f4be2615257e7a8bb5a6322c
HuanGong <gonghuan.dev@gmail.com>
2024-12-03 13:44:21 +0800

    float right side

--- nvim/lua/config/nvimcmp.lua
+++ nvim/lua/config/nvimcmp.lua
@@ -22,9 +22,8 @@ local has_words_before = function()
         return false
     end
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
     -- stylua: ignore
-    -- return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local formating_fomat = function(entry, item)
@@ -76,8 +75,8 @@ cmp.setup({
 
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() and has_words_before() then
-                -- cmp.select_next_item()
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_next_item()
+                -- cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
                 -- luasnap: wee use c-j/c-k to jump to next/previous item
                 -- elseif luasnip.locally_jumpable(1) then
                 --     luasnip.jump(1)
--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -25,8 +25,71 @@ local function on_attach(bufnr)
     vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
 end
 
+local enable_float = true
+
 local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.64 -- You can change this too
+
+local function center_float_view()
+    local screen_w = vim.opt.columns:get()
+    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+    local window_w = screen_w * WIDTH_RATIO
+    local window_h = screen_h * HEIGHT_RATIO
+    local window_w_int = math.floor(window_w)
+    local window_h_int = math.floor(window_h)
+    local center_x = (screen_w - window_w) / 2
+    local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+
+    return {
+        -- relativenumber = true,
+        adaptive_size = true,
+        -- ref: https://github.com/MarioCarrion/videos/tree/main/2023
+        float = {
+            enable = enable_float,
+            open_win_config = {
+                border = "rounded",
+                relative = "editor",
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+            }
+        },
+        width = window_w_int,
+    }
+end
+
+local function right_float_view()
+    local screen_w = vim.opt.columns:get()
+    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+    local window_w = 32
+    local window_h = screen_h * 0.95
+    local window_w_int = math.floor(window_w)
+    local window_h_int = math.floor(window_h)
+
+    local center_x = (screen_w - window_w) - 2
+    local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+
+    return {
+        -- relativenumber = true,
+        adaptive_size = true,
+        -- ref: https://github.com/MarioCarrion/videos/tree/main/2023
+        side = "right",
+        float = {
+            enable = enable_float,
+            open_win_config = {
+                border = "rounded",
+                relative = "editor",
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+            }
+        },
+        width = window_w_int,
+    }
+end
 
 nvimtree.setup({
     hijack_netrw = true,
@@ -43,39 +106,15 @@ nvimtree.setup({
         enable = false,
     },
     filters = {
-      custom = { "^.git$" },
+        custom = { "^.git$" },
     },
     diagnostics = {
         enable = true,
     },
-    view = {
-        -- relativenumber = true,
-        adaptive_size = true,
-        -- ref: https://github.com/MarioCarrion/videos/tree/main/2023
-        float = {
-            enable = true,
-            open_win_config = function()
-                local screen_w = vim.opt.columns:get()
-                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-                local window_w = screen_w * WIDTH_RATIO
-                local window_h = screen_h * HEIGHT_RATIO
-                local window_w_int = math.floor(window_w)
-                local window_h_int = math.floor(window_h)
-                local center_x = (screen_w - window_w) / 2
-                local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                    - vim.opt.cmdheight:get()
-                return {
-                    border = "rounded",
-                    relative = "editor",
-                    row = center_y,
-                    col = center_x,
-                    width = window_w_int,
-                    height = window_h_int,
-                }
-            end,
+    view = right_float_view(),
+    actions = {
+        open_file = {
+            quit_on_open = false,
         },
-        width = function()
-            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-        end,
     },
 })
