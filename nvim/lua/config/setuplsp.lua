5d6a255088eed3e2c55cf89433450d0fe576a557
gonghuan <gonghuan@bytedance.com>
2025-07-30 22:35:50 +0800

    feat: go.nvim

--- nvim/lua/config/setuplsp.lua
+++ nvim/lua/config/setuplsp.lua
@@ -36,37 +36,28 @@ formatutil.setup({
     idlfmt_ftypes = {},
 })
 
-vim.cmd("command! Rename lua vim.lsp.buf.rename()<cr>")
--- vim.cmd('command! Format lua vim.lsp.buf.format({async = true})<cr>')
-vim.api.nvim_create_user_command('LspFormat', function()
+vim.api.nvim_create_user_command('Format', function()
     local bufnr = vim.api.nvim_get_current_buf()
     formatutil.format_with_priority(bufnr, true, 3000, "null-ls")
 end, {})
-vim.cmd('command! LspSignsHelp lua vim.lsp.buf.signature_help()<cr>')
-vim.cmd('command! LspDocSymbols lua vim.lsp.buf.document_symbol()<cr>')
-vim.cmd('command! LspOutGoingCalls lua vim.lsp.buf.outgoing_calls()<cr>')
-vim.cmd('command! LspInComingCalls lua vim.lsp.buf.incoming_calls()<cr>')
-
-local shared_on_attach = function(client, bufnr)
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_buf_set_keymap
-    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    keymap(bufnr, "n", "gs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts)
-    keymap(bufnr, "n", "sh", "<cmd>lua vim.lsp.buf.signature_help({async = true})<cr>", opts)
-    -- lsp action instruction
-    keymap(bufnr, "n", "F", "<cmd>LspCodeAction <cr>", opts)
-    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-
-    vim.notify("lsp [" .. client.name .. "] on_attach", vim.log.levels.INFO)
-
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if status_ok then
-        illuminate.on_attach(client)
-    end
-end
+vim.api.nvim_create_user_command("Hover", function ()
+    vim.lsp.buf.hover()
+end, {})
+vim.api.nvim_create_user_command('Hover', function()
+    vim.lsp.buf.hover()
+end, {})
+vim.api.nvim_create_user_command('Rename', function()
+    vim.lsp.buf.rename()
+end, {})
+vim.api.nvim_create_user_command('LspDocSymbols', function()
+    vim.lsp.buf.document_symbol()
+end, {})
+vim.api.nvim_create_user_command('LspOutGoingCalls', function()
+    vim.lsp.buf.outgoing_calls()
+end, {})
+vim.api.nvim_create_user_command('LspInComingCalls', function()
+    vim.lsp.buf.incoming_calls()
+end, {})
 
 require("lspconfig")
 require("mason").setup()
@@ -84,6 +75,7 @@ require("mason-lspconfig").setup({
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('ltvim.lsp', {}),
     callback = function(args)
+        local bufnr = args.buf
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
         -- version = "*", 0.11 兼容问题, https://github.com/ray-x/lsp_signature.nvim/pull/355
@@ -96,6 +88,28 @@ vim.api.nvim_create_autocmd('LspAttach', {
         --     }
         -- }, 0)
 
-        shared_on_attach(client, args.buf)
+        local keymap = vim.keymap.set
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        local ok, tb = pcall(require, "telescope.builtin")
+        if ok then
+            keymap('n', 'gr', tb.lsp_references, opts)
+            keymap('n', 'gd', tb.lsp_definitions, opts)
+            keymap('n', 'gi', tb.lsp_implementations, opts)
+            keymap('n', 'gs', tb.lsp_document_symbols, opts)
+        else
+            keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+            keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+            keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+            keymap("n", "gs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts)
+        end
+        keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+        -- lsp action instruction
+        keymap("n", "F", "<cmd>LspCodeAction <cr>", opts)
+        keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+
+        local status_ok, illuminate = pcall(require, "illuminate")
+        if status_ok then
+            illuminate.on_attach(client)
+        end
     end,
 })
