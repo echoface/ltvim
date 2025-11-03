97fc6dbfdb74e73c8a29ff507daac7e0a57cab5c
gonghuan <gonghuan@bytedance.com>
2025-11-03 23:08:46 +0800

    feat: add codecompanion

---
+++ nvim/lua/lazynvim/codecompanion.lua
@@ -0,0 +1,48 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
+        "MeanderingProgrammer/render-markdown.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+        -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+        require("codecompanion").setup({
+            display = {
+                chat = {
+                    show_settings = true,
+                },
+            },
+            adapters = {
+                acp = {
+                    claude_code = function()
+                        return require("codecompanion.adapters").extend("claude_code", {
+                            env = {
+                                ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_AUTH_TOKEN"),
+                            },
+                        })
+                    end,
+                },
+            },
+            strategies = {
+                chat = {
+                    adapter = "claude_code",
+                },
+                inline = {
+                    adapter = "claude_code",
+                },
+                cmd = {
+                    adapter = "claude_code",
+                }
+            }
+        })
+    end,
+    opts = {},
+    keys = {
+        { "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", desc = "codecompanion chat" },
+        { "<leader>ca", "<cmd>CodeCompanionActions<cr>",     desc = "toggle code action" },
+    },
+}
