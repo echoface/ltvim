5ff4002acd0881bbb3a3016ccc8721c0525aa6b7
gonghuan <gonghuan@bytedance.com>
2025-12-09 22:30:51 +0800

    fix: jj in terminal mode

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -85,7 +85,6 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
     -- pattern = { "terminal", "toggleterm", "snacks_terminal" },
     callback = function()
         local opts = { buffer = 0 }
-        vim.keymap.set('t', 'jj', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 
