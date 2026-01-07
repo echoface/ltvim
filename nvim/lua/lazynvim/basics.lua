93534a998e24b4d2c2212233d05c19a3ab82ad54
gonghuan <gonghuan@bytedance.com>
2026-01-07 13:17:59 +0800

    fix: theme

--- nvim/lua/lazynvim/basics.lua
+++ nvim/lua/lazynvim/basics.lua
@@ -3,43 +3,47 @@ return {
     { "moll/vim-bbye",                event = "VeryLazy" },
     { "APZelos/blamer.nvim",          event = "VeryLazy" },
     { "f-person/auto-dark-mode.nvim", opts = {} },
-    { "savq/melange-nvim", priority=1000, config=function ()
-        vim.cmd.colorscheme("melange")
-    end },
-    -- {
-    --     "loctvl842/monokai-pro.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         require("monokai-pro").setup({
-    --             day_night = {
-    --                 enable = true,        -- turn off by default
-    --                 day_filter = "light", -- classic | octagon | pro | machine | ristretto | spectrum
-    --                 night_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-    --             },
-    --         })
-    --     end
-    -- },
-    -- {
-    --     'sainnhe/sonokai',
-    --     -- priority = 1000,
-    --     config = function()
-    --         vim.g.sonokai_style = 'espresso'
-    --         -- 'default', 'atlantis', 'andromeda', 'shusia', 'maia', 'espresso'
-    --         vim.g.sonokai_enable_italic = true
-    --         vim.g.sonokai_better_performance = 1
-    --         -- vim.cmd.colorscheme('sonokai')
-    --     end
-    -- },
-    -- {
-    --     "morhetz/gruvbox",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.g.gruvbox_bold = 1
-    --         vim.g.gruvbox_italic = 1
-    --         vim.g.gruvbox_contrast_light = "soft"
-    --         -- vim.cmd.colorscheme("gruvbox")
-    --     end,
-    -- },
+    {
+        "savq/melange-nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("melange")
+        end
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+        priority = 1000,
+        config = function()
+            require("monokai-pro").setup({
+                day_night = {
+                    enable = true,        -- turn off by default
+                    day_filter = "light", -- classic | octagon | pro | machine | ristretto | spectrum
+                    night_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+                },
+            })
+        end
+    },
+    {
+        'sainnhe/sonokai',
+        -- priority = 1000,
+        config = function()
+            vim.g.sonokai_style = 'espresso'
+            -- 'default', 'atlantis', 'andromeda', 'shusia', 'maia', 'espresso'
+            vim.g.sonokai_enable_italic = true
+            vim.g.sonokai_better_performance = 1
+            -- vim.cmd.colorscheme('sonokai')
+        end
+    },
+    {
+        "morhetz/gruvbox",
+        -- priority = 1000,
+        config = function()
+            vim.g.gruvbox_bold = 1
+            vim.g.gruvbox_italic = 1
+            vim.g.gruvbox_contrast_light = "soft"
+            -- vim.cmd.colorscheme("gruvbox")
+        end,
+    },
     {
         'rcarriga/nvim-notify',
         config = function()
