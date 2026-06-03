21734e43ef083a3004d8b264c9675a6b76c1dbb7
gonghuan <gonghuan@bytedance.com>
2026-06-03 11:37:06 +0800

    feat: start insert for term

--- nvim/lua/lazynvim/toggleterm.lua
+++ nvim/lua/lazynvim/toggleterm.lua
@@ -5,8 +5,11 @@ return {
     config = function()
         require("toggleterm").setup({
             direction = 'float',
-            persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+            persist_mode = false, -- 每次重新打开都回到默认模式，而不是记住上次的 normal/insert 状态
             start_in_insert = true,
+            on_open = function(_)
+                vim.cmd("startinsert!")
+            end,
             float_opts = {
                 -- width = vim.o.columns, -- Adjust the width
                 -- height = math.floor(vim.o.lines * 0.6),  -- Adjust the height
@@ -21,4 +24,3 @@ return {
         vim.keymap.set({"n"}, "tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
     end
 }
-
