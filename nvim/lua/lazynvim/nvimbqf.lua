c3c6495d32896e1a0432ebd9b130af97ce8bdfea
gonghuan <gonghuan@bytedance.com>
2026-06-26 11:11:39 +0800

    feat: add quicker, disable bqf

--- nvim/lua/lazynvim/nvimbqf.lua
+++ nvim/lua/lazynvim/nvimbqf.lua
@@ -1,69 +1,8 @@
--- local def = {
---     auto_enable = true,
---     magic_window = true,
---     auto_resize_height = false,
---     previous_winid_ft_skip = {},
---     preview = {
---         auto_preview = true,
---         border = 'rounded',
---         show_title = true,
---         show_scroll_bar = true,
---         delay_syntax = 50,
---         winblend = 12,
---         win_height = 15,
---         win_vheight = 15,
---         wrap = false,
---         buf_label = true,
---         should_preview_cb = nil
---     },
---     func_map = {
---         open = '<CR>',
---         openc = 'o',
---         drop = 'O',
---         split = '<C-x>',
---         vsplit = '<C-v>',
---         tab = 't',
---         tabb = 'T',
---         tabc = '<C-t>',
---         tabdrop = '',
---         ptogglemode = 'zp',
---         ptoggleitem = 'p',
---         ptoggleauto = 'P',
---         pscrollup = '<C-b>',
---         pscrolldown = '<C-f>',
---         pscrollorig = 'zo',
---         prevfile = '<C-p>',
---         nextfile = '<C-n>',
---         prevhist = '<',
---         nexthist = '>',
---         lastleave = [['"]],
---         stoggleup = '<S-Tab>',
---         stoggledown = '<Tab>',
---         stogglevm = '<Tab>',
---         stogglebuf = [['<Tab>]],
---         sclear = 'z<Tab>',
---         filter = 'zn',
---         filterr = 'zN',
---         fzffilter = 'zf'
---     },
---     filter = {
---         fzf = {
---             action_for = {
---                 ['ctrl-t'] = 'tabedit',
---                 ['ctrl-v'] = 'vsplit',
---                 ['ctrl-x'] = 'split',
---                 ['ctrl-q'] = 'signtoggle',
---                 ['ctrl-c'] = 'closeall'
---             },
---             extra_opts = {'--bind', 'ctrl-o:toggle-all'}
---         }
---     }
--- }
 return {
     "kevinhwang91/nvim-bqf",
     lazy    = false,
     ft      = { "qf" },
-    enabled = true,
+    enabled = false,
     version = "*",
     opts    = {
         auto_enable = true,
